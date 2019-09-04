1762e7f1ecd3b62066d96429c011749bd4a4c7aa
Jeff Williams <jeff@cybergrits.com>
2019-09-04 16:58:58 -0500

    Fixed various logic probems and updated timestamp used

--- HX-Dedup-Hosts/HX-Dedup-Hosts.ps1
+++ HX-Dedup-Hosts/HX-Dedup-Hosts.ps1
@@ -1,5 +1,5 @@
 ﻿<#
-.SYNOPSIS
+.SYSNOPSIS
     Removes hosts in HX with duplicate hostnames.
  
 .DESCRIPTION
@@ -7,13 +7,13 @@
     It will sort and identify the host with the most recent last_poll_timestamp
     and delete the rest from HX.
  
-.PARAMETER
+.PARAMETERS
     Server: IP address or URL of the HX server
  
     Port: Port number the HX server is listening on
  
 .EXAMPLE
-    C:\> HX-Dedup-Hosts.ps1 -Server x.x.x.x -Port ####
+    C:\> HX-Dedup-Hosts.ps1 -Server x.x.x.x-Port ####
  
 .NOTES   
     Author: Jeff Williams
@@ -22,13 +22,14 @@
 #>
  
  
-
+ 
 param(
+    [parameter(Mandatory=$true)]
     [string]$Server,
-    [string]$Port
+    [string]$Port = "3000"
     )
  
-$FireEyeUrl = "https://$Server"+":$Port"
+$Url = "https://$Server"+":$Port"
 $timestamp = Get-Date -Format yyyyMMdd-HHmm
  
  
@@ -75,12 +76,16 @@ Function Ignore-SelfSignedCerts {
         }
  
     [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-    }
+}
  
  
 Function HX-Auth {
 # Authenticates to the HX Server and returns a user Token
- 
+    # Define Parameters
+    Param(
+        [Parameter(Mandatory)]
+        [string]$URL
+    )
     # Prompts for and processes API user creds   
     $c = Get-Credential
     $cpair = "$($c.username):$($c.GetNetworkCredential().Password)"
@@ -90,59 +95,76 @@ Function HX-Auth {
     $header = @{
         "Accept" = "application/json"
         "Authorization" = "Basic $key"
-        }
+    }
    
     # Authenticates to the HX server
-    $gettoken = Invoke-WebRequest -Uri "$FireEyeUrl/hx/api/v3/token" -Headers $header -Method Get
+    $gettoken = Invoke-WebRequest -Uri "$Url/hx/api/v3/token" -Headers $header -Method Get
  
     $token = $gettoken.Headers.'X-FeApi-Token'
     $token
+}
  
-    }
  
- 
-Function HX-DeAuth($Token) {
+Function HX-DeAuth {
 # Logs off API user of supplied Token
- 
+    #Define Parameters
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$URL,
+        [Parameter(Mandatory=$true)]
+        [string]$Token
+    )
     # Required Header info
     $header = @{
         "Accept" = "application/json"
         "X-FeApi-Token" = "$Token"
-        }
-   
-    $apiLogOff = Invoke-WebRequest -Uri "$FireEyeUrl/hx/api/v3/token" -Headers $header -Method Delete
- 
-    $apiLogOff
- 
     }
+   
+    $apiLogOff = Invoke-WebRequest -Uri "$URL/hx/api/v3/token" -Headers $header -Method Delete
+    if($apiLogOff.StatusCode -eq "204"){Write-Host "User Successfully Logged Off" -ForegroundColor Cyan}     
+}
  
  
-Function HX-Get-Hosts($Token) {
+Function HX-Get-Hosts {
 # Gets Info for All hosts in HX
- 
+    #Define Parameters
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$URL,
+        [Parameter(Mandatory=$true)]
+        [string]$Token,
+        [string]$Limit='65000'
+    )
+
     # Required Header info
     $header = @{
         "Accept" = "application/json"
         "X-FeApi-Token" = "$Token"
-        }
-    # Gets info on all hosts in HX (Notice the "...?limit=35000" and
-    # increase/decrease depending on number of agents in HX)    #>
-    $FireEyeHosts = Invoke-RestMethod -Uri "$FireEyeUrl/hx/api/v3/hosts?limit=35000" -Headers $header -Method Get
- 
-    $FireEyeHosts
- 
     }
+    # Gets info on all hosts in HX 
+    $FireEyeHosts = Invoke-RestMethod -Uri "$URL/hx/api/v3/hosts?limit=$Limit" -Headers $header -Method Get
+    $FireEyeHosts
+ }
  
  
-Function HX-Delete-Host($AgentID,$Token){
+Function HX-Delete-Host{
 # Delete Host based off Agent ID
- 
+    #Define Parameters
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$URL,
+        [Parameter(Mandatory=$true)]
+        [string]$Token,
+        [Parameter(Mandatory=$true)]
+        [string]$AgentID
+    )
+
     $header = @{
         "Accept" = "application/json"
         "X-FeApi-Token" = "$Token"
         }
    
-    $DeleteHost = Invoke-RestMethod -Uri "$FireEyeUrl/hx/api/v3/hosts/$AgentID" -Headers $header -Method Delete
+    $DeleteHost = Invoke-RestMethod -Uri "$URL/hx/api/v3/hosts/$AgentID" -Headers $header -Method Delete
        
     $DeleteHost.StatusCode
     }
@@ -152,59 +174,71 @@ Function HX-Delete-Host($AgentID,$Token){
 Ignore-SelfSignedCerts
  
 # Logs into the HX server and gets a user token
-$Token = HX-Auth
+$Token = HX-Auth -Url $Url
  
 # Gets Host info from the HX server
-$hxGet = HX-Get-Hosts -Token $Token
- 
-# Creates and Organizes a usful host list from the data recieved
-Write-Host "Getting Host info from HX" -ForegroundColor Cyan
-$hosts = $hxGet.data.entries | select _id,agent_version,hostname,last_poll_timestamp |
-    Sort-Object -Property hostname,last_poll_timestamp -Descending
- 
-# Identifies unique hosts   
-$uniqueHosts = $hosts | Sort-Object -Unique hostname -Descending
- 
-# Creates a list of just the duplicate hosts
-$rhosts= @()
-$c1 = 0
-Foreach($h in $hosts){
-   
-    $c1++
-    Write-Progress -Activity "Identifying Duplicates" -Status "Processing" -PercentComplete (($c1 / $hosts.Count)*100)
- 
-    if($h -notin $uniqueHosts){
-        $rhosts += $h
+write-host "Getting Host information from Endpoint Security" -ForegroundColor Cyan
+$hxGet = (HX-Get-Hosts -Token $Token -URL $Url).data.entries | select _id,hostname,last_audit_timestamp
+Write-Host "$($hxGet.Count) Hosts in HX `n" -ForegroundColor Green
+
+# Formats Last audit timestamp to a number
+Write-Host "Formatting Data" -ForegroundColor Cyan
+$hxGet | foreach {$_.last_audit_timestamp = $_.last_audit_timestamp -replace"-","" -replace"T","" -replace".000Z",""}
+$hxGet | foreach {$_.last_audit_timestamp = $_.last_audit_timestamp.ToInt64($null)}
+
+# Finds and Groups Duplicate Hostnames
+Write-host "Identifing hosts with duplicate hostname" -ForegroundColor Cyan
+$hg=$hxGet | Group-Object -Property hostname | where -FilterScript {$_.count -gt 1}
+
+# Identifies latest agent based off last_audit_timestamp
+$latestAgent = @()
+$duplicateHosts=@()
+
+foreach($h in $hg){
+    # Finds latest timestamp
+    $latestTimestamp = ($h.Group | measure -Maximum -Property last_audit_timestamp).Maximum
+
+    # Places Host in proper group
+    foreach($d in $h.Group){
+        if($d.last_audit_timestamp -eq $latestTimestamp){
+            $latestAgent += $d
         }
-   
+        Else{$duplicateHosts += $d}
     }
- 
- 
-# Verifies that you would like to remove the hosts from HX
-# and saves a log file if you continue.
-Write-Host "You are about to remove $($rhosts.Count) hosts from HX." -ForegroundColor Red
-$continue = Read-Host "Do you wish to continue? (y/n): "
- 
-if($continue -ieq 'y'){
+}
+$newCount=$latestAgent.count
+$dupCount=$duplicateHosts.Count
+Write-Host "Found $newCount hosts with duplicate agents" -ForegroundColor Green
+Write-Host "Identified $dupCount duplicate hosts `n" -ForegroundColor Yellow
+
+# Verifies that you would like to remove the dups from HX
+# and saves a log file if you continue
+Write-Host "You are about to remove $dupCount hosts from HX." -ForegroundColor Red
+$continue = Read-Host "Do you wish to continue? (y/n)"
+$timestamp = Get-Date -Format yyyyMMdd-HHmm
+
+if($continue -eq 'y'){
     # Removes hosts
-    $c2 = 0
-    Foreach($id in $rhosts._id){
-   
-        $c2++
-        Write-Progress -Activity "Removing Hosts from HX" -Status "Processing" -PercentComplete (($c2 / $rhosts.Count)*100)
- 
-        HX-Delete-Host -AgentID $id -Token $Token
- 
-        }
-    Write-Host "Removed $($rhosts.Count) hosts from HX" -ForegroundColor Green
-   
-    # Saves Log File
-    $sfile = saveFile -Title "Save Log File As" -SaveAs "HostsRemovedFromHX-$timestamp.csv"
-    $rhosts | Export-Csv -Path $sfile -NoTypeInformation
+    $c1=0
+    foreach($id in $duplicateHosts._id){
+        $c1++
+        Write-Progress -Activity "Removing hosts from HX" -Status "Processing $c1 of $dupCount" -PercentComplete (($c1/$dupCount)*100)
+
+        HX-Delete-Host -AgentID $id -URL $Url -Token $Token
     }
+    Write-Host "Removed $dupCount hosts from HX" -ForegroundColor Cyan
+
+    $logFile = saveFile -Title "Save Log File As" -SaveAs "HostsDeleted-HX-$timestamp.csv"
+    $duplicateHosts | Export-Csv -Path $logFile -NoTypeInformation
+}
 Else{
+    # Saves all logs
+    $logFile = saveFile -Title "Save Log File As" -SaveAs "HostsFrom-HX-"
+    $hg.Group | Export-Csv -Path "$logFile-RawExport.csv" -NoTypeInformation
+    $latestAgent | Export-Csv -Path "$logFile-LatestAgent.csv" -NoTypeInformation
+    $duplicateHosts | Export-Csv -Path "$logFile-Duplicates.csv" -NoTypeInformation
     Write-Host "Process Aborted: $timestamp" -ForegroundColor Red
-    }
- 
-# Logs out of HX
-HX-DeAuth -Token $Token
+}
+
+# Closes API session with HX
+HX-DeAuth -URL $Url -Token $Token
